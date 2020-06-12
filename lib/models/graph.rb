4d8418f48e5af5ad9127203e66176f5858818c25
Juan Guecaimburu <guecaimburu.j@gmail.com>
2020-06-12 09:50:38 +0200

    Correct graph exposure as argument when adding edge. Change graph structure to facilitate solving flow.

--- lib/models/graph.rb
+++ lib/models/graph.rb
@@ -6,30 +6,33 @@ class Graph
   def initialize(edges = [])
     raise TypeError unless edges.respond_to?(:each)
 
-    @graph = build_graph_from(edges)
+    build_graph_from(edges)
   end
 
-  def add_edge(edge, graph = @graph)
+  def add_edge(edge)
     raise TypeError unless edge[:from] && edge[:to]
 
-    try_to_add_edge(edge, graph)
+    try_to_add_edge(edge)
     rescue TypeError
       puts edge
       raise TypeError, 'Element is not an edge'
   end
 
+  def bulk_add_edges(edges)
+    edges.each { |edge| add_edge(edge) }
+  end
+
   private
 
   def build_graph_from(edges)
-    edges.inject({}) do |graph, edge|
-      add_edge(edge, graph)
-    end
+    @graph = { from: {} }
+    bulk_add_edges(edges)
   end
 
-  def try_to_add_edge(edge, graph)
-    graph[edge[:from]] ||= []
-    graph[edge[:to]] ||= []
-    graph[edge[:from]] << edge
-    graph
+  def try_to_add_edge(edge)
+    @graph[:from][edge[:from]] ||= { to: {} }
+    @graph[:from][edge[:to]] ||= { to: {} }
+    @graph[:from][edge[:from]][:to][edge[:to]] = edge[:data]
+    @graph
   end
 end
--- spec/models/graph_spec.rb
+++ spec/models/graph_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/models/graph'
 
 RSpec.describe Graph do
   it 'is initialized with no argument' do
-    expect { Graph.new() }.not_to raise_error
+    expect { Graph.new }.not_to raise_error
   end
 
   it 'is initialized with empty iterable' do
@@ -25,28 +25,43 @@ RSpec.describe Graph do
 
   it 'creates a graph with edges as lists at nodes keys' do
     edges = [
-      { from: 1, to: 2 },
-      { from: 2, to: 3 },
-      { from: 2, to: 4 },
-      { from: 3, to: 4 }
+      { from: 1, to: 2, data: '' },
+      { from: 2, to: 3, data: '' },
+      { from: 2, to: 4, data: '' },
+      { from: 3, to: 4, data: '' }
     ]
     graph = Graph.new(edges)
-    expect(graph.graph[1].count).to eq(1)
-    expect(graph.graph[2].count).to eq(2)
-    expect(graph.graph[3].count).to eq(1)
-    expect(graph.graph[4].count).to eq(0)
+    expect(graph.graph[:from][1][:to].size).to eq(1)
+    expect(graph.graph[:from][2][:to].size).to eq(2)
+    expect(graph.graph[:from][3][:to].size).to eq(1)
+    expect(graph.graph[:from][4][:to].size).to eq(0)
   end
 
   it 'adds edge correctly' do
-    edge = { from: 1, to: 2 }
-    graph = Graph.new()
+    edge = { from: 1, to: 2, data: '' }
+    graph = Graph.new
     graph.add_edge(edge)
-    expect(graph.graph[1].count).to eq(1)
+    expect(graph.graph[:from][1][:to].size).to eq(1)
   end
 
   it 'raises error if given object is not an edge' do
     edge = { foo: 1, bar: 2 }
-    graph = Graph.new()
+    graph = Graph.new
     expect{ graph.add_edge(edge) }.to raise_error(TypeError)
+  end
+
+  it 'add bulk of edges' do
+    graph = Graph.new
+    edges = [
+      { from: 1, to: 2, data: '' },
+      { from: 2, to: 3, data: '' },
+      { from: 2, to: 4, data: '' },
+      { from: 3, to: 4, data: '' }
+    ]
+    graph.bulk_add_edges(edges)
+    expect(graph.graph[:from][1][:to].size).to eq(1)
+    expect(graph.graph[:from][2][:to].size).to eq(2)
+    expect(graph.graph[:from][3][:to].size).to eq(1)
+    expect(graph.graph[:from][4][:to].size).to eq(0)
   end
 end
